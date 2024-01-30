05e1be2c0d2ee1c110a5de781494710a042a80c8
Geane Ramos <geaneramos1995@gmail.com>
2024-01-30 09:03:41 -0300

    feat: feed photos

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import FeedModal from './FeedModal'
+import FeedPhotos from './FeedPhotos'
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
+  return <div>Feed
+    <FeedModal />
+    <FeedPhotos />
+  </div>
+  
 }
 
 export default Feed
