ff5ec328c60cf95935cc508d0d071470deb2083a
Geane Ramos <geaneramos1995@gmail.com>
2024-02-05 08:00:01 -0300

    feat: photo comments

--- src/Components/Photo/PhotoComments.jsx
+++ src/Components/Photo/PhotoComments.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import { UserContext } from '../../UserContext'
-import PhotoCommentsForm from './FotoCommentsForm'
+import PhotoCommentsForm from './PhotoCommentsForm'
+import styles from './PhotoComments.module.css'
 
 export const PhotoComments = (props) => {
   const {login} = React.useContext(UserContext)
-  
-  return <div>
-    { login && <PhotoCommentsForm id={props.id}/>}
-  </div>
+  const [comments, setComments] = React.useState(() => props.comments)
+ 
+  return <>
+    <ul className={styles.comments}>
+      {comments.map(comment => <li key={comment.comment_ID}>
+        <b>{comment.comment_author}:</b>
+        <span>{comment.comment_content}</span>
+      </li>)}
+    </ul>
+    { login && <PhotoCommentsForm id={props.id} setComments={setComments}/>}
+  </>
   
 }
 
