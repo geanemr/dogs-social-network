0f9027e3d37846ed00465c805f8b55b6c57f398a
Geane Ramos <geaneramos1995@gmail.com>
2024-02-11 09:12:24 -0300

    feat: error page

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,30 +1,32 @@
-import React from 'react'
-import { Routes, Route, useNavigate } from 'react-router-dom'
-import LoginForm from './LoginForm'
-import LoginCreate from './LoginCreate'
-import LoginPasswordLost from './LoginPasswordLost'
-import LoginPasswordReset from './LoginPasswordReset'
-import { UserContext } from '../../UserContext'
-import styles from './Login.module.css'
+import React from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import LoginForm from "./LoginForm";
+import LoginCreate from "./LoginCreate";
+import LoginPasswordLost from "./LoginPasswordLost";
+import LoginPasswordReset from "./LoginPasswordReset";
+import { UserContext } from "../../UserContext";
+import styles from "./Login.module.css";
+import NotFound from "../NotFound";
 
 const Login = () => {
-  const navigate = useNavigate()
-  const {login} = React.useContext(UserContext);
+  const navigate = useNavigate();
+  const { login } = React.useContext(UserContext);
 
-  if(login === true) return navigate("/conta");
-  
+  if (login === true) return navigate("/conta");
+
   return (
     <section className={styles.login}>
       <div className={styles.forms}>
-      <Routes>
-        <Route path='/' element={<LoginForm />} />
-        <Route path='criar' element={<LoginCreate />} />
-        <Route path='perdeu' element={<LoginPasswordLost />} />
-        <Route path='resetar' element={<LoginPasswordReset />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="criar" element={<LoginCreate />} />
+          <Route path="perdeu" element={<LoginPasswordLost />} />
+          <Route path="resetar" element={<LoginPasswordReset />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
