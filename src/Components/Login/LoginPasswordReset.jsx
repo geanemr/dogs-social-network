e4c90c1db1738223c309e50d4a46a7e90734dcfd
Geane Ramos <geaneramos1995@gmail.com>
2024-02-13 18:28:09 -0300

    style: improvements

--- src/Components/Login/LoginPasswordReset.jsx
+++ src/Components/Login/LoginPasswordReset.jsx
@@ -36,7 +36,7 @@ const LoginPasswordReset = () => {
     }
   }
   return (
-    <div>
+    <section className="animeLeft">
       <Head title="Resete a senha"/>
       <h1 className="title">Resete a senha</h1>
       <form onSubmit={handleSubmit}>
@@ -48,7 +48,7 @@ const LoginPasswordReset = () => {
         )}
       </form>
       <Error error={error} />
-    </div>
+    </section>
   );
 };
 
