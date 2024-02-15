d2119e2e9b706c0fcdac4e0b0d3fa55a1bd51d4d
Geane Ramos <geaneramos1995@gmail.com>
2024-02-15 10:41:35 -0300

    fix: setloading

--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -56,7 +56,7 @@ export const UserStorage = ({ children }) => {
       if (token) {
         try {
           setError(null);
-          setLogin(true);
+          setLoading(true);
           const { url, options } = TOKEN_VALIDATE_POST(token);
           const response = await fetch(url, options);
           if (!response.ok) throw Error("Token Inválido");
